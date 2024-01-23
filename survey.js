70c2fdd4f42332b9dda940f165079d057441dea1
Ashley Barclift <abar15@vt.edu>
2024-01-23 15:20:19 -0800

    push

---
+++ survey.js
@@ -0,0 +1,18 @@
+
+function survey()
+{
+    var n,c,l;window.SMCX=window.SMCX||[],
+    document.getElementById("smcx-sdk")||
+    (
+        n=document.getElementsByTagName("script"),
+        c=n[n.length-1],
+        l=document.createElement("script"),
+        l.type="text/javascript",
+        l.async=!0,
+        l.id="smcx-sdk",
+        l.src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd0zO8UO_2FEJ7AOu7T0CjpbqDPd8_2Fo_2BlEUlkmMNmS9P_2BFc.js",
+        c.parentNode.insertBefore(l,c)
+    )
+}
+
+
